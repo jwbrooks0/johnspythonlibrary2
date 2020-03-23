2d75c92a813f35f6c17255b73f5d987dc0ff653c
John Brooks <jwbrooks0@gmail.com>
2020-03-23 17:52:37 -0400

    Added a template file for _settings.py

---
+++ ReadWrite/_settingsTemplate.py
@@ -0,0 +1,9 @@
+###########################################################
+# Populate the following variables with your desired values.
+# When done editing this file, rename it to _settings.py and leave it in the same directory. 
+
+### local directory to save data.  
+# This is relevant to the decorator called _backupDFs in ReadWrite.  As downloading data from a remote server is typically a bottlebeck, this decorator allows data to be automatically stored locally on your computer.  This variable is the filepath where this data is saved.  Make sure to create the directory and python will not make it for you.  I leave it as an empty string as it personal to each user.  Also, an empty string will cause the data to be redownloaded each time should you not wish to save the data locally.
+
+localDirToSaveData=''
+
