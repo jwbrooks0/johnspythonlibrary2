c8d673112bfd02c3c39886bb1a441b443cb0edd2
John Brooks <jwbrooks0@gmail.com>
2020-03-23 17:56:42 -0400

    added a template file for _settings.py

---
+++ Hbtep/Get/_settingsTemplate.py
@@ -0,0 +1,14 @@
+###########################################################
+# Populate the following variables with your desired values.
+# When done editing this file, rename it to _settings.py and leave it in the same directory. 
+# DO NOT MAKE CHANGES TO THE ORIGINAL TEMPLATE FILE AND THEN PUSH IT.  
+
+### Server address
+# This is the internet address of the HBTEP server
+_SERVER_ADDRESS=''
+
+### Server port
+# This is the internet port of the HBTEP server
+_SERVER_PORT=-1
+
+
