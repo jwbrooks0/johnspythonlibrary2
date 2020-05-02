1f22c746f647cf05ea4eb51bb6d09bd8da613b62
John Brooks <jwbrooks0@gmail.com>
2020-05-02 15:46:03 -0400

    Changed how the Hbtep library is loaded.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 
 
-from . import Hbtep
+try:
+	# This file does not exist by default.  Rename the template file and fill in the appropriate variables within.
+	from . import Hbtep
+except ImportError:
+	print(Warning('Warning: Hbtep/Get/_settings.py file not found. \nHave you modified the template file yet in Hbtep/Get/? \nHave you installed the mdsplus library? \nUntil you do, the Hbtep sub-library will be disabled.'))
+	
+
 from . import Plot
 from . import Process
 from . import ReadWrite
