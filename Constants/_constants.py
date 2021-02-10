848187fcfd55a17ac6d178faa37d12c9e6ffafab
jwbrooks0 <jwbrooks0@gmail.com>
2021-02-10 14:11:21 -0500

    minor renaming of variables

--- Constants/_constants.py
+++ Constants/_constants.py
@@ -6,11 +6,11 @@ Created on Mon Feb  8 09:24:10 2021
 """
 
 # %% scipy constants
-from scipy.constants import pi, c, mu_0, h, hbar, G, g, e, R, N_A, k, m_e, m_p, m_n
+from scipy.constants import pi, c, mu_0, h, hbar, G, g, e, R, N_A, k as k_b, m_e, m_p, m_n
 
 # %% Misc
 amu = 1.66054e-27 			# amu to kg
-ev = 1.60218e-19			# ev to Joule
+eV = e * 1.0				# ev to Joule
 ep_0 = 1.0 / (mu_0 * c**2)  # vacuum permittivity
 
 # %% Atomic masses
