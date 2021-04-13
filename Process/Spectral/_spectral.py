cb9bdda209ffd5b7a4de9228a25968f008d2b210
jwbrooks0 <jwbrooks0@gmail.com>
2021-04-12 20:18:58 -0400

    dispersion_plot() created.

--- Process/Spectral/_spectral.py
+++ Process/Spectral/_spectral.py
@@ -3,6 +3,8 @@ import numpy as _np
 import pandas as _pd
 import matplotlib.pyplot as _plt
 from scipy import fftpack as _fftpack
+from scipy.signal import welch as _welch
+from scipy.signal.spectral import _spectral_helper
 from johnspythonlibrary2 import Plot as _plot
 from johnspythonlibrary2.Plot import subTitle as _subTitle, finalizeFigure as _finalizeFigure, finalizeSubplot as _finalizeSubplot
 from johnspythonlibrary2.Process.Misc import check_dims as _check_dims
@@ -14,8 +16,98 @@ import xarray as _xr
 ###############################################################################
 #%% Dispersion plots
 
-def dispersion_plot(da, dim1='t', dim2='theta', nperseg_dim1=None, nperseg_dim2=None, plot=True):
-	print('work in progress')
+
+def dispersion_plot(video_data_1D, nperseg_dim1=1000,  dim2='theta', dim2_final='m', vmin=None, vmax=None, plot=True, f_units='Hz'):
+	"""
+	Calculates a dispersion plot from a 1D video dataset
+	
+	Parameters
+	----------
+	video_data_1D : xarray.core.dataarray.DataArray
+		1D video data.  dims = ['t', spatial (e.g. theta or r)].  Time must be first.
+	nperseg_dim1 : int or None
+		int - Welch FFT averaging is applied to the time data where nperseg is the window size.  The output will be real.
+		None - Standard FFT is applied to the time data (i.e. no windowing).  The output will be complex.
+	dim2 : str
+		The name of the spatial dimension
+	dim2_final : str
+		The name of the spatial dimension after the FFT is applied
+	vmin : float
+		Lower limit of the colorbar scale
+	vmax : float
+		Upper limit of the colorbar scale
+	plot : bool
+		True causes the plot to be produced.
+	f_units : str
+		Name of the frequency units.  (e.g. if t=t*1e3 is the input, then specify f_units='kHz'.)
+		
+	Returns
+	-------
+	X_2D : xarray.core.dataarray.DataArray
+		Dipserion relationship.  Values are real if nperseg_dim1 is a number.  Complex if nperseg_dim1 is None.
+	"""
+	## Check dimensions
+	_check_dims(video_data_1D, dims=['t',dim2])
+	if video_data_1D.dims[0]!='t':
+		raise Exception("The first dimension needs to be time, 't'")
+
+	## FFT along dim2 (the spatial dimension)
+	if True: 
+		# preliminary steps
+		dtheta = float(video_data_1D[dim2][1] -
+					   video_data_1D[dim2][0]) / (2 * _np.pi)
+		m = _fftpack.fftfreq(len(video_data_1D[dim2]), d=dtheta)
+	
+		# perform FFT
+		X = _np.fft.fft(video_data_1D, axis=1)
+		X = _xr.DataArray(X, dims=['t', dim2_final],
+							coords=[video_data_1D['t'], m]).sortby(dim2_final)
+	
+		# return the results to the correct amplitude
+		N = len(video_data_1D[dim2])
+		X *= 1.0 / N  # use 2.0/N only if you've trimmed the negative freqs
+
+	## FFT along time, t (dim1)
+	if True:
+		
+		# preliminary steps
+		dt = float(X.t[1] - X.t[0])
+		
+		# perform time-averaged (windowed) FFT if  nperseg_dim1 is a number
+		if nperseg_dim1 is not None:
+			freq, X_2D = _welch( 	X.data, fs=1.0/dt, nperseg=nperseg_dim1,
+									noverlap=nperseg_dim1//2, return_onesided=True,
+									scaling='spectrum', axis=0)
+		# otherwise, perform standard fft 
+		else: 
+					freq = _fftpack.fftfreq(len(X['t']), d=dt)
+					X_2D = _np.fft.fft(X.data, axis=0)
+					N = len(video_data_1D['t'])
+					X_2D *= 1.0 / N  # use 2.0/N only if you've trimmed the negative freqs
+
+		X_2D = _xr.DataArray(X_2D, dims=['f', dim2_final],
+							coords=[freq, X[dim2_final]]).sortby('f')
+		X_2D.attrs={'long_name':'Spectral density','units':'au'}
+		X_2D.f.attrs={'long_name':'FFT Frequency','units':f_units}
+		X_2D[dim2_final].attrs={'long_name': dim2_final,'units':''}
+	
+	if plot==True:
+		# convert to absolute value and take log10 (for vetter visualization)
+		a=_np.log10(_np.abs(X_2D))
+		a.attrs={'long_name':'Spectral density','units':'au, log10'}
+		
+		# set vmin and vmax (color scaling limits)
+		if type(vmin)==type(None):
+			vmin=float(a.min())
+		if type(vmax)==type(None):
+			vmax=float(a.max())#+0.5
+		
+		# plot
+		fig, ax = _plt.subplots()
+		a.plot(ax=ax, vmin=vmin, vmax=vmax)
+		ax.set_title('dispersion plot')
+	
+	return X_2D
 	
 	
 def dispersion_plot_2points(da1, da2, x_separation=1, nperseg=None, plot=True):
