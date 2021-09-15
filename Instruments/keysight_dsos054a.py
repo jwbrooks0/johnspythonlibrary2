c962664e869e18eb053334f3fbce371e60e6cf1f
John Brooks <34584188+jwbrooks0@users.noreply.github.com>
2021-09-15 11:21:31 -0400

    github desktop is broken.  forcing updates

--- Instruments/keysight_dsos054a.py
+++ Instruments/keysight_dsos054a.py
@@ -1,10 +1,3 @@
-#!python3
-# *********************************************************
-# This program illustrates a few commonly-used programming
-# features of your Keysight Infiniium Series oscilloscope.
-# *********************************************************
-# Import modules.
-# ---------------------------------------------------------
 # import visa
 import pyvisa
 # import string
@@ -13,263 +6,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
-# # Global variables (booleans: 0 = False, 1 = True).
-# # ---------------------------------------------------------
-# debug = 0
-# # =========================================================
-# # Initialize:
-# # =========================================================
-# def initialize():
-# 	# Clear status.
-# 	do_command("*CLS")
-# 	# Get and display the device's *IDN? string.
-# 	idn_string = do_query_string("*IDN?")
-# 	print("Identification string: '%s'" % idn_string)
 
-# 	# Load the default setup.
-# 	do_command("*RST")
-# 	
-# # =========================================================
-# # Capture:
-# # =========================================================
-# def capture():
-# 	# Set probe attenuation factor.
-# # 	do_command(":CHANnel1:PROBe 1.0")
-# 	qresult = do_query_string(":CHANnel1:PROBe?")
-# 	print("Channel 1 probe attenuation factor: %s" % qresult)
-# 	# Use auto-scale to automatically set up oscilloscope.
-# 	print("Autoscale.")
-# 	do_command(":AUToscale")
-# 	# Set trigger mode.
-# 	do_command(":TRIGger:MODE EDGE")
-# 	qresult = do_query_string(":TRIGger:MODE?")
-# 	print("Trigger mode: %s" % qresult)
-# 	# Set EDGE trigger parameters.
-# 	do_command(":TRIGger:EDGE:SOURce CHANnel1")
-# 	qresult = do_query_string(":TRIGger:EDGE:SOURce?")
-# 	print("Trigger edge source: %s" % qresult)
-# # 	do_command(":TRIGger:LEVel CHANnel1,330E-3")
-# 	qresult = do_query_string(":TRIGger:LEVel? CHANnel1")
-# 	print("Trigger level, channel 1: %s" % qresult)
-# 	do_command(":TRIGger:EDGE:SLOPe POSitive")
-# 	qresult = do_query_string(":TRIGger:EDGE:SLOPe?")
-# 	print("Trigger edge slope: %s" % qresult)
-# 	# Save oscilloscope setup.
-# # 	sSetup = do_query_ieee_block(":SYSTem:SETup?")
-# # 	f = open("setup.stp", "wb")
-# # 	f.write(sSetup)
-# # 	f.close()
-# # 	print("Setup bytes saved: %d" % len(sSetup))
-# 	# Change oscilloscope settings with individual commands:
-# 	# Set vertical scale and offset.
-# 	do_command(":CHANnel1:SCALe 0.1")
-# 	qresult = do_query_number(":CHANnel1:SCALe?")
-# 	print("Channel 1 vertical scale: %f" % qresult)
-# 	do_command(":CHANnel1:OFFSet 0.0")
-# 	qresult = do_query_number(":CHANnel1:OFFSet?")
-# 	print("Channel 1 offset: %f" % qresult)
-# 	# Set horizontal scale and offset.
-# 	do_command(":TIMebase:SCALe 200e-6")
-# 	
-# 	qresult = do_query_string(":TIMebase:SCALe?")
-# 	print("Timebase scale: %s" % qresult)
-# 	do_command(":TIMebase:POSition 0.0")
-# 	qresult = do_query_string(":TIMebase:POSition?")
-# 	print("Timebase position: %s" % qresult)
-# 	# Set the acquisition mode.
-# 	do_command(":ACQuire:MODE RTIMe")
-# 	qresult = do_query_string(":ACQuire:MODE?")
-# 	print("Acquire mode: %s" % qresult)
-# # 	# Or, set up oscilloscope by loading a previously saved setup.
-# # 	sSetup = ""
-# # 	f = open("setup.stp", "rb")
-# # 	sSetup = f.read()
-# # 	f.close()
-# # 	do_command_ieee_block(":SYSTem:SETup", sSetup)
-# # 	print("Setup bytes restored: %d" % len(sSetup))
-# 	# Set the desired number of waveform points,
-# 	# and capture an acquisition.
-# 	do_command(":ACQuire:POINts 32000")
-# 	do_command(":DIGitize")
-# 	
-# 	
-# # def get_all_data():
-# # 	
-# # 	return get_data(1),get_data(2),get_data(3),get_data(4)
-
-# # def get_data(ch,plot=True):
-# # 	
-# # 	print(do_query_string(":WAVeform:POINts?"))
-# # 	do_command(":ACQuire:POINts 32000")
-# # 	print(do_query_string(":WAVeform:POINts?"))
-# # # 	do_command(":DIGitize")
-# # 	
-# # 	do_command(":WAVeform:SOURce CHANnel%s"%ch)
-# # 	do_command(":MEASure:SOURce CHANnel%s"%ch)
-# # # 	do_command(":WAVeform:SOURce CHANnel1")
-# # 	
-# # 	print(do_query_string(":WAVeform:POINts?"))
-# # 	do_command(":WAVeform:FORMat BYTE")	
-# # 	
-# # 	print(do_query_string(":WAVeform:POINts?"))
-# # # 	do_command(":MEASure:SOURce CHANnel1")
-# # # 	qresult = do_query_string(":MEASure:SOURce?")
-# # # 	print("Measure source: %s" % qresult)
-# # 	
-# # 	print( do_query_string(":WAVeform:SOURce?"))
-# # 	print( do_query_string(":MEASure:SOURce?"))
-# # 		# Get the waveform data.
-# # 	do_command(":WAVeform:STReaming OFF")
-# # 	sData = do_query_ieee_block(":WAVeform:DATA?",data_points=int(do_query_string(":WAVeform:POINts?")))
-# # 	# Unpack signed byte data.
-# # 	values = struct.unpack("%db" % len(sData), np.array(sData).astype(np.byte))
-# # 	# print("Number of data values: %d" % len(values))
-# # 	
-# # 	x_increment = do_query_number(":WAVeform:XINCrement?")
-# # 	x_origin = do_query_number(":WAVeform:XORigin?")
-# # 	y_increment = do_query_number(":WAVeform:YINCrement?")
-# # 	y_origin = do_query_number(":WAVeform:YORigin?")
-# # 	
-# # 	data = np.array(values) * y_increment + y_origin
-# # 	time = np.arange(len(data)) * x_increment + x_origin
-
-# # 	import xarray as xr
-# # 	data=xr.DataArray(	data,
-# # 							 dims='t',
-# # 							 coords=[time])
-# # 	
-# # 	if plot==True:
-# # 		fig,ax=plt.subplots()
-# # 		data.plot(ax=ax)
-# # 	return data
-# # =========================================================
-# # Analyze:
-# # =========================================================
-# def analyze():
-# 	# Make measurements.
-# 	# --------------------------------------------------------
-# 	do_command(":MEASure:SOURce CHANnel1")
-# 	qresult = do_query_string(":MEASure:SOURce?")
-# 	print("Measure source: %s" % qresult)
-# 	do_command(":MEASure:FREQuency")
-# 	qresult = do_query_string(":MEASure:FREQuency?")
-# 	print("Measured frequency on channel 1: %s" % qresult)
-# 	do_command(":MEASure:VAMPlitude")
-# 	qresult = do_query_string(":MEASure:VAMPlitude?")
-# 	print("Measured vertical amplitude on channel 1: %s" % qresult)
-# # 	# Download the screen image.
-# # 	# --------------------------------------------------------
-# # 	sDisplay = do_query_ieee_block(":DISPlay:DATA? PNG")
-# # 	# Save display data values to file.
-# # 	f = open("screen_image.png", "wb")
-# # 	f.write(sDisplay)
-# # 	f.close()
-# # 	print("Screen image written to screen_image.png.")
-# 	# Download waveform data.
-# 	# --------------------------------------------------------
-# 	
-# 	# Get the waveform type.
-# 	qresult = do_query_string(":WAVeform:TYPE?")
-# 	print("Waveform type: %s" % qresult)
-# 	# Get the number of waveform points.
-# 	qresult = do_query_string(":WAVeform:POINts?")
-# 	print("Waveform points: %s" % qresult)
-# 	# Set the waveform source.
-# 	do_command(":WAVeform:SOURce CHANnel1")
-# 	qresult = do_query_string(":WAVeform:SOURce?")
-# 	print("Waveform source: %s" % qresult)
-# 	# Choose the format of the data returned:
-# 	do_command(":WAVeform:FORMat BYTE")
-# 	print("Waveform format: %s" % do_query_string(":WAVeform:FORMat?"))
-# 	# Display the waveform settings from preamble:
-# 	wav_form_dict = {
-# 	0 : "ASCii",
-# 	1 : "BYTE",
-# 	2 : "WORD",
-# 	3 : "LONG",
-# 	4 : "LONGLONG",
-# 	}
-# 	acq_type_dict = {
-# 	1 : "RAW",
-# 	2 : "AVERage",
-# 	3 : "VHIStogram",
-# 	4 : "HHIStogram",
-# 	6 : "INTerpolate",
-# 	10 : "PDETect",
-# 	}
-# 	acq_mode_dict = {
-# 	0 : "RTIMe",
-# 	1 : "ETIMe",
-# 	3 : "PDETect",
-# 	}
-# 	coupling_dict = {
-# 	0 : "AC",
-# 	1 : "DC",
-# 	2 : "DCFIFTY",
-# 	3 : "LFREJECT",
-# 	}
-# 	units_dict = {
-# 	0 : "UNKNOWN",
-# 	1 : "VOLT",
-# 	2 : "SECOND",
-# 	3 : "CONSTANT",
-# 	4 : "AMP",
-# 	5 : "DECIBEL",
-# 	}
-# 	preamble_string = do_query_string(":WAVeform:PREamble?")
-# 	(
-# 	wav_form, acq_type, wfmpts, avgcnt, x_increment, x_origin,
-# 	x_reference, y_increment, y_origin, y_reference, coupling,
-# 	x_display_range, x_display_origin, y_display_range,
-# 	y_display_origin, date, time, frame_model, acq_mode,
-# 	completion, x_units, y_units, max_bw_limit, min_bw_limit
-# 	) = preamble_string.split(",")
-# 	print("Waveform format: %s" % wav_form_dict[int(wav_form)])
-# 	print("Acquire type: %s" % acq_type_dict[int(acq_type)])
-# 	print("Waveform points desired: %s" % wfmpts)
-# 	print("Waveform average count: %s" % avgcnt)
-# 	print("Waveform X increment: %s" % x_increment)
-# 	print("Waveform X origin: %s" % x_origin)
-# 	print("Waveform X reference: %s" % x_reference) # Always 0.
-# 	print("Waveform Y increment: %s" % y_increment)
-# 	print("Waveform Y origin: %s" % y_origin)
-# 	print("Waveform Y reference: %s" % y_reference) # Always 0.
-# 	print("Coupling: %s" % coupling_dict[int(coupling)])
-# 	print("Waveform X display range: %s" % x_display_range)
-# 	print("Waveform X display origin: %s" % x_display_origin)
-# 	print("Waveform Y display range: %s" % y_display_range)
-# 	print("Waveform Y display origin: %s" % y_display_origin)
-# 	print("Date: %s" % date)
-# 	print("Time: %s" % time)
-# 	print("Frame model #: %s" % frame_model)
-# 	print("Acquire mode: %s" % acq_mode_dict[int(acq_mode)])
-# 	print("Completion pct: %s" % completion)
-# 	print("Waveform X units: %s" % units_dict[int(x_units)])
-# 	print("Waveform Y units: %s" % units_dict[int(y_units)])
-# 	print("Max BW limit: %s" % max_bw_limit)
-# 	print("Min BW limit: %s" % min_bw_limit)
-# 	# Get numeric values for later calculations.
-# 	x_increment = do_query_number(":WAVeform:XINCrement?")
-# 	x_origin = do_query_number(":WAVeform:XORigin?")
-# 	y_increment = do_query_number(":WAVeform:YINCrement?")
-# 	y_origin = do_query_number(":WAVeform:YORigin?")
-# 	# Get the waveform data.
-# 	do_command(":WAVeform:STReaming OFF")
-# 	sData = do_query_ieee_block(":WAVeform:DATA?")
-# 	# Unpack signed byte data.
-# 	import numpy as np
-# 	values = struct.unpack("%db" % len(sData), np.array(sData).astype(np.byte))
-# 	print("Number of data values: %d" % len(values))
-# 	# Save waveform data values to CSV file.
-# 	f = open("waveform_data.csv", "w")
-# 	for i in range(0, len(values) - 1):
-# 		time_val = x_origin + (i * x_increment)
-# 		voltage = (values[i] * y_increment) + y_origin
-# 		f.write("%E, %f\n" % (time_val, voltage))
-# 	f.close()
-# 	print("Waveform format BYTE data written to waveform_data.csv.")
-# 	
-# 	
 			
 class keysight_dsos054a:
 	""" 
@@ -284,8 +21,8 @@ class keysight_dsos054a:
 	"""
 	
 	def __init__(	self,
-					address="TCPIP0::192.168.0.246::inst0::INSTR",
-					timeout=20000,
+					address="TCPIP0::192.168.0.247::inst0::INSTR",
+					timeout=60000,
 					debug=False):
 		self.address=address
 		self.timeout=timeout
@@ -932,7 +669,6 @@ class keysight_dsos054a:
 			
 		return ds	
 	
-	
 	def get_data_multichannel(self, chs=[1, 2, 3, 4], ch_names=[], scaling=[], plot=False):
 		""" download all four channels of data """
 		
@@ -965,8 +701,7 @@ class keysight_dsos054a:
 		
 		return ds	
 	
-	# %% set oscope operation
-	
+	# %% "set" oscope operation
 	def set_SINGLE(self):
 		self.do_command('SINGle')
 		
@@ -975,7 +710,31 @@ class keysight_dsos054a:
 	
 	def set_RUN(self):
 		self.do_command('RUN')
-	
+		
+	# %% save data
+	def save_data_locally(self, dir_and_filename='c:\data', file_format='H5INt'):
+		"""
+		Instructs the scope to write data locally (to its own harddrive or usb drive).
+		See page 493 in https://www.keysight.com/us/en/assets/9018-07141/programming-guides/9018-07141.pdf
+
+		Parameters
+		----------
+		dir_and_filename : str
+			The directory and filename (not including the file extension) to write the data to
+		file_format : str
+			Fileformat of the output data. 
+			
+			'H5INt' (Default).  HDF5 format saved as ints.  Needs to be multiplied by the y-increment and added to y-origin in order to properly scale to volts
+			
+			'H5'.  HDF5 format saved as floats.
+
+		Returns
+		-------
+		None.
+
+		"""
+		self.do_command(':DISK:SAVE:WAVeform ALL,"%s",%s,OFF' % (dir_and_filename, file_format))
+		
 	
 # %% main
 
