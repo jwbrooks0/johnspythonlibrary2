c15369503032bdec68886a1b32a8817ef77e9d2f
jwbrooks0 <jwbrooks0@gmail.com>
2021-09-25 21:12:39 -0400

    Removed the Pandas sublibrary

--- Process/Pandas/__init__.py
+++
@@ -1,1 +0,0 @@
-from ._pandas import *
--- Process/__init__.py
+++ Process/__init__.py
@@ -2,7 +2,6 @@ from . import EDM
 from . import Filters
 from . import Fit
 from . import Misc
-from . import Pandas
 from . import Spectral
 from . import Statistics
 from . import TF
