2e623df0f1c8b392ca3c592a04de655592daf847
jwbrooks0 <jwbrooks0@gmail.com>
2021-09-25 21:26:57 -0400

    Cleaned up

--- Process/Filters/_filters.py
+++ Process/Filters/_filters.py
@@ -1,9 +1,7 @@
 
 import numpy as _np
-# import pandas as _pd
 import matplotlib.pyplot as _plt
 from johnspythonlibrary2.Plot import finalizeSubplot as _finalizeSubplot
-# from johnspythonlibrary2.Process.SigGen import chirp as _chirp
 import xarray as _xr
 
 
@@ -138,15 +136,15 @@ def filtfilt(	da,
 		else:
 			raise Exception('Invalid input type')
 	if 'time' in da.dims:
-		da=da.rename({'time':'t'})
+		da = da.rename({'time': 't'})
 	if 't' not in da.dims:
 		raise Exception('time not present')
 		
-	from scipy.signal import butter,filtfilt
+	from scipy.signal import butter, filtfilt
 	
 	# construct butterworth filter
-	samplingFreq=float(1.0/(da.t[1]-da.t[0]))
-	Wn=_np.array(cornerFreq).astype(float)/samplingFreq*2  # I don't know why this factor of 2 needs to be here
+	samplingFreq = float(1.0 / (da.t[1] - da.t[0]))
+	Wn = _np.array(cornerFreq).astype(float) / samplingFreq * 2  # I don't know why this factor of 2 needs to be here
 	b, a = butter(	filterOrder, 
 					Wn=Wn,
 					btype=filterType,
@@ -157,20 +155,20 @@ def filtfilt(	da,
 	daOut = _xr.DataArray(	filtfilt(	b, a, da.values.reshape(-1),
 										),
 							   dims='t',
-							   coords={'t':da.t})
+							   coords={'t': da.t})
 	
-	if plot==True:
-		fig,ax=_plt.subplots()
-		da.plot(ax=ax,label='original')
-		daOut.plot(ax=ax,label='filtered')
-		_finalizeSubplot(ax,
-					   xlabel='Time',
-					   ylabel='Signal amplitude',)
+	if plot is True:
+		fig, ax = _plt.subplots()
+		da.plot(ax=ax, label='original')
+		daOut.plot(ax=ax, label='filtered')
+		_finalizeSubplot(	ax,
+							xlabel='Time',
+							ylabel='Signal amplitude')
 		
 	return daOut
 		
 		
-def gaussianFilter(da,timeFWHM,filterType='high',plot=False):
+def gaussianFilter(da, timeFWHM, filterType='high', plot=False):
 	"""
 	Low and highpass filters using scipy's gaussian convolution filter
 	
@@ -232,7 +230,7 @@ def gaussianFilter(da,timeFWHM,filterType='high',plot=False):
 	if type(da) not in [_xr.core.dataarray.DataArray]:
 		raise Exception('Invalid input type')
 	if 'time' in da.dims:
-		da=da.rename({'time':'t'})
+		da = da.rename({'time':'t'})
 	if 't' not in da.dims:
 		raise Exception('time not present')
 	
@@ -240,11 +238,11 @@ def gaussianFilter(da,timeFWHM,filterType='high',plot=False):
 	from scipy.ndimage import gaussian_filter1d
 	
 	# convert FWHM to standard deviation
-	dt=float(da.t[1]-da.t[0])
-	def fwhmToGaussFilterStd(fwhm,dt=dt):
-		std=1.0/_np.sqrt(8*_np.log(2))*fwhm/dt
+	dt = float(da.t[1] - da.t[0])
+	def fwhmToGaussFilterStd(fwhm, dt=dt):
+		std = 1.0 / _np.sqrt(8 * _np.log(2)) * fwhm / dt
 		return std
-	std=fwhmToGaussFilterStd(timeFWHM,dt)
+	std = fwhmToGaussFilterStd(timeFWHM, dt)
 	
 	# perform gaussian filter
 	daFiltered=_xr.DataArray(	gaussian_filter1d(	da,
@@ -252,118 +250,25 @@ def gaussianFilter(da,timeFWHM,filterType='high',plot=False):
 													axis=0,
 													mode='nearest'),
 								dims=['t'],
-								coords={'t':da.t})
+								coords={'t': da.t})
 
 	# optional plot of results
-	if plot==True:
-		fig,ax=_plt.subplots()
+	if plot is True:
+		fig, ax = _plt.subplots()
 		da.plot(ax=ax,label='Raw')
-		daFiltered.plot(ax=ax,label='Low-pass')
-		(da-daFiltered).plot(ax=ax,label='High-pass')
+		daFiltered.plot(ax=ax, label='Low-pass')
+		(da - daFiltered).plot(ax=ax, label='High-pass')
 		_finalizeSubplot(	ax,
 							xlabel='Time',
 						    ylabel='Signal amplitude',)
 
-	if filterType=='low':
+	if filterType == 'low':
 		return daFiltered
-	elif filterType=='high':
+	elif filterType == 'high':
 		return da-daFiltered
 	else:
 		raise Exception('Bad filter type')
 
-
-def gaussianFilter_df(df,timeFWHM,filterType='high',plot=False):
-	"""
-	Low and pass filters using scipy's gaussian convolution filter
-	
-	Parameters
-	----------
-	df  :  pandas.core.frame.DataFrame
-		data = single or multiple array s
-		index = time
-	timeFWHM : float
-		full width at half maximum of the gaussian with units in time.  this
-		effectively sets the corner frequency of the filter
-	filterType : str
-		'high' - high-pass filter
-		'low' - low-pass filter
-	plot : bool
-		plots the results
-	plotGaussian : bool
-		plots the gaussian distribution used for the filter
-		
-	Returns
-	-------
-	 : pandas.core.frame.DataFrame
-		data = filtered verion of df
-		same index and columns as df
-		
-	Examples
-	--------
-	Example1::
-		
-		t=_np.arange(0,100e-3-4e-6,2e-6)
-		fStart=2e2
-		fStop=2.0e3
-		y1=_chirp(t,[10e-3,90e-3],[fStart,fStop])#[1e-3,19.46e-3]
-		df=_pd.DataFrame(y1,
-					  index=t,
-					  columns=['orig']) 
-		
-		fwhm=0.5e-3
-		dfHP=gaussianFilter_df(df,fwhm,'high',plot=False)
-		dfHP.columns=['HP']
-		dfLP=gaussianFilter_df(df,fwhm,'low',plot=False)
-		dfLP.columns=['LP']
-		
-		fig,ax=_plt.subplots()
-		ax.plot(df,label='Original')
-		ax.plot(dfHP,label='Highpass')
-		ax.plot(dfLP,label='Lowpass')
-		_finalizeSubplot(ax,
-					   xlabel='Time',
-					   ylabel='Signal amplitude',)
-		
-		
-	References
-	----------
-	https://en.wikipedia.org/wiki/Full_width_at_half_maximum
-	https://docs.scipy.org/doc/scipy-0.19.0/reference/generated/scipy.signal.gaussian.html
-	https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.gaussian_filter1d.html
-	"""
-	
-	# import
-	from scipy.ndimage import gaussian_filter1d
-	import pandas as _pd
-	
-	# time step
-	dt=df.index[1]-df.index[0]
-	
-	# convert FWHM to standard deviation
-	def fwhmToGaussFilterStd(fwhm,dt=dt):
-		std=1.0/_np.sqrt(8*_np.log(2))*fwhm/dt
-		return std
-	std=fwhmToGaussFilterStd(timeFWHM,dt)
-	
-	# perform gaussian filter
-	dfFiltered=_pd.DataFrame(gaussian_filter1d(df,std,axis=0,mode='nearest'),index=df.index,columns=df.columns)
-
-	# optional plot of results
-	if plot==True:
-		for i,(key,val) in enumerate(df.iteritems()):
-			_plt.figure()
-			_plt.plot(val.index,val,label='Raw')
-			_plt.plot(dfFiltered[key].index,dfFiltered[key],label='Low-pass')
-			_plt.plot(val.index,(df-dfFiltered)[key],label='High-pass')
-			_plt.legend()
-			_plt.title(key)
-
-	if filterType=='low':
-		return dfFiltered
-	elif filterType=='high':
-		return df-dfFiltered
-	else:
-		raise Exception('Bad filter type')
 
 def butterworth(	da,
 					cornerFreq,
@@ -455,19 +360,19 @@ def butterworth(	da,
 		if type(da) in [_np.ndarray]:
  			da=_xr.DataArray(	da,
 								dims=['t'],
-								coords={'t':time})
+								coords={'t': time})
 		else:
  			raise Exception('Invalid input type')
 	if 'time' in da.dims:
-		da=da.rename({'time':'t'})
+		da = da.rename({'time': 't'})
 	if 't' not in da.dims:
 		raise Exception('time not present')
 		
-	from scipy.signal import butter,lfilter
+	from scipy.signal import butter, lfilter
  	
  	# construct butterworth filter
-	samplingFreq=float(1.0/(da.t[1]-da.t[0]))
-	Wn=_np.array(cornerFreq).astype(float)/samplingFreq*2  # I don't know why this factor of 2 needs to be here
+	samplingFreq = float(1.0 / (da.t[1] - da.t[0]))
+	Wn = _np.array(cornerFreq).astype(float) / samplingFreq * 2  # I don't know why this factor of 2 needs to be here
 	b, a = butter(	filterOrder, 
  					Wn=Wn,
  					btype=filterType,
@@ -477,15 +382,15 @@ def butterworth(	da,
 	# perform forwards-backwards filter
 	daOut = _xr.DataArray(	lfilter(b, a, da.values.reshape(-1)),
 							dims='t',
-							coords={'t':da.t})
+							coords={'t': da.t})
 	
-	if plot==True:
-		fig,ax=_plt.subplots()
-		da.plot(ax=ax,label='original')
-		daOut.plot(ax=ax,label='filtered')
-		_finalizeSubplot(ax,
-					   xlabel='Time',
-					   ylabel='Signal amplitude',)
+	if plot is True:
+		fig ,ax = _plt.subplots()
+		da.plot(ax=ax, label='original')
+		daOut.plot(ax=ax, label='filtered')
+		_finalizeSubplot(	ax,
+							xlabel='Time',
+							ylabel='Signal amplitude',)
 		
 	return daOut
 	
