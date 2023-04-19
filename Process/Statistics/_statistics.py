36b68b28c14d41c9355a077ed4ac67c391a07286
John Brooks <jwbrooks0@gmail.com>
2023-04-19 14:02:40 -0400

    Added earth_mover_distance_v4()

--- Process/Statistics/_statistics.py
+++ Process/Statistics/_statistics.py
@@ -4,7 +4,102 @@ import pandas as _pd
 import scipy as _sp
 import xarray as _xr
 import matplotlib.pyplot as _plt
+import ot as _ot
 
+def earth_mover_distance_v4(da1, da2, plot=False):
+	
+	if _np.any(da1.values < 0) or _np.any(da2.values < 0):
+		raise Exception("Values must be positive")
+	
+	if False:
+		def noise(M, amp):
+			return (_np.random.rand(M) - 0.5) * amp
+		
+		def gaussian(x, sigma, x0=0):
+		    """ Return the normalized Gaussian with standard deviation sigma. """
+		    # c = _np.sqrt(2 * _np.pi)
+		    return _np.exp(-0.5 * ((x - x0) / sigma)**2) #  / (_np.sqrt(2 * _np.pi) * _np.abs(sigma))
+		
+		
+		x = _np.linspace(-1.25, 1.25, 1000)
+		x = _xr.DataArray(x, coords={'x': x})
+		sigma = 0.05
+		
+		noise_amp = 0.0001
+		x0_array = _np.linspace(-1, 1, 75)
+		x0_array = _xr.DataArray(x0_array, coords={"x0": x0_array})
+		emd_array = x0_array * 0.0
+		
+		_plt.ioff()
+		def make_plot(da1, da2, emd_result, filename):
+				
+			fig, ax = _plt.subplots(2, sharex=True)
+			ax[0].axvline(0, ls="--", color="grey", lw=0.5)
+			ax[1].axvline(0, ls="--", color="grey", lw=0.5)
+			da1.plot(ax=ax[0], label="Distribution 1")
+			da2.plot(ax=ax[0], label="Distribution 2")
+			ax[0].legend()
+			ax[0].set_ylabel("Gaussian distributions")
+			if len(emd_result) > 1:
+				emd_result.plot(ax=ax[1])
+			ax[0].set_ylim([0, 1.5])
+			ax[1].set_ylim([0, 1.1])
+			ax[0].set_xlim([-1.25, 1.25])
+			ax[0].set_title("")
+			ax[1].set_xlabel("x")
+			ax[1].set_ylabel("EMD result")
+			fig.set_tight_layout(True)
+			fig.savefig(filename, dpi=100)
+			_plt.close(fig)
+			
+		for i, x0 in enumerate(x0_array):
+			da1 = gaussian(x, sigma=sigma, x0=0) + _np.abs(noise(len(x), noise_amp))
+			da2 = gaussian(x, sigma=sigma, x0=x0) + _np.abs(noise(len(x), noise_amp))
+			emd_array[i] = earth_mover_distance_v4(da1, da2)
+			make_plot(da1, da2, emd_result=emd_array[:(i+1)], filename="%.4d.png" % i)
+				
+		import imageio
+		from glob import glob
+		import os
+		
+		## create animation file
+		files = glob("*.png")
+		ims = [imageio.imread(f) for f in files]
+		imageio.mimwrite("animation.gif", ims)
+		
+		## delete all png files
+		for file in files:
+			os.remove(file)
+	
+	
+	## normalize each signal such that integral of each = 1.0
+	da1 = da1 / da1.sum()
+	da2 = da2 / da2.sum()
+	
+	## isolate coordinates
+	coords1 = da1[list(da1.coords.keys())[0]].to_numpy()
+	coords1 = coords1.reshape((len(coords1), 1))
+	coords2 = da1[list(da1.coords.keys())[0]].to_numpy()
+	coords2 = coords2.reshape((len(coords2), 1))
+	
+	## calculate euclidean distance between each set of coordinates
+	M = _ot.dist(x1=coords1, x2=coords2, metric='euclidean') # euclidean distance from each pair of points to every other pair of points
+	norm = M.max()
+	M /= norm
+	
+	## perform EMD
+	G0 = _ot.emd(a=da1.values, b=da2.values, M=M)
+	emd_result = _np.sum(_np.sum(_np.multiply(G0, M))) * norm
+	
+	if plot is True:
+		
+		fig, ax = _plt.subplots()
+		da1.plot(ax=ax, label="da1")
+		da2.plot(ax=ax, label="da2")
+		ax.legend()
+		ax.set_title("EMD = %.3e" % emd_result)
+	
+	return emd_result
 
 
 def histogram(values_np, 
